b608fd5a20d5fd3342cbd8c0e1da0034f75b8883
Yasas Wickramarathne <61660117+yasaswick@users.noreply.github.com>
2020-06-15 00:06:29 +0530

    published

--- lib/screen/detail_page.dart
+++ lib/screen/detail_page.dart
@@ -19,7 +19,6 @@ class DetailPage extends StatelessWidget {
               height: 350,
               child: Stack(
                 children: <Widget>[
-                  
                   Container(
                       width: MediaQuery.of(context).size.width,
                       height: 300,
@@ -27,18 +26,18 @@ class DetailPage extends StatelessWidget {
                         imageUrl: succulent.img,
                         fit: BoxFit.cover,
                       )),
-                //       Container(
-                //   decoration: BoxDecoration(
-                //       gradient: LinearGradient(
-                //      begin: FractionalOffset.bottomCenter,
-                //     end: FractionalOffset.topCenter,
-                //     colors: [
-                //       Colors.black.withOpacity(0.0),
-                //       Colors.black,
-                //     ],
-                //   )),
-                //   height: 200,
-                // ),
+                  //       Container(
+                  //   decoration: BoxDecoration(
+                  //       gradient: LinearGradient(
+                  //      begin: FractionalOffset.bottomCenter,
+                  //     end: FractionalOffset.topCenter,
+                  //     colors: [
+                  //       Colors.black.withOpacity(0.0),
+                  //       Colors.black,
+                  //     ],
+                  //   )),
+                  //   height: 200,
+                  // ),
                   Positioned(
                     child: Container(
                       height: 50,
@@ -59,68 +58,78 @@ class DetailPage extends StatelessWidget {
                     ),
                   ),
                   Positioned(
-                      left: 20,
-                      top: 250,
-                      child: Container(
-                        padding: EdgeInsets.all(20),
-                        decoration: BoxDecoration(
-                        boxShadow: [BoxShadow(color: Colors.black54 , blurRadius: 5, spreadRadius: 5 , offset: Offset(0, 0) )],
-                        color: banner,
-                        borderRadius: BorderRadius.circular(3)),
-                          child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: <Widget>[
-                              Text(
-                                this.succulent.name ?? '',
-                                style: TextStyle(
-                                    fontSize: 22,
-                                    fontWeight: FontWeight.w700,
-                                    color: Colors.white),
-                              ),
-                              SizedBox(height: 5),
-                              Text(
-                                this.succulent.sname ?? '',
-                                overflow: TextOverflow.ellipsis,
-                                style: TextStyle(
-                                  
-                                    fontSize: 18, color: Colors.white70),
-                              ),
-                            ],
+                    left: 20,
+                    top: 250,
+                    child: Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(boxShadow: [
+                        BoxShadow(
+                            color: Colors.black54,
+                            blurRadius: 5,
+                            spreadRadius: 5,
+                            offset: Offset(0, 0))
+                      ], color: banner, borderRadius: BorderRadius.circular(3)),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          Text(
+                            this.succulent.name ?? '',
+                            style: TextStyle(
+                                fontSize: 22,
+                                fontWeight: FontWeight.w700,
+                                color: Colors.white),
                           ),
-                          height: 100,
-                          width: MediaQuery.of(context).size.width - 40,
-                        ),
-                      )
+                          SizedBox(height: 5),
+                          Text(
+                            this.succulent.sname ?? '',
+                            overflow: TextOverflow.ellipsis,
+                            style:
+                                TextStyle(fontSize: 18, color: Colors.white70),
+                          ),
+                        ],
+                      ),
+                      height: 100,
+                      width: MediaQuery.of(context).size.width - 40,
+                    ),
+                  )
                 ],
               ),
             ),
             Container(
-              padding: EdgeInsets.symmetric(horizontal: 20, vertical: 30),
+              padding: EdgeInsets.symmetric(vertical: 30),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(
-                        'Introduction',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w500),
-                      ),
-                      SizedBox(height: 10),
-                      Text(
-                        this.succulent.description,
-                        style: TextStyle(fontSize: 16),
-                      ),
-                      SizedBox(height: 20),
-                      Text(
-                        'Details',
-                        style: TextStyle(
-                            fontSize: 18, fontWeight: FontWeight.w500),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: <Widget>[
+                            Text(
+                              'Introduction',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                            SizedBox(height: 10),
+                            Text(
+                              this.succulent.description,
+                              style: TextStyle(fontSize: 16),
+                            ),
+                            SizedBox(height: 20),
+                            Text(
+                              'Details',
+                              style: TextStyle(
+                                  fontSize: 18, fontWeight: FontWeight.w500),
+                            ),
+                          ],
+                        ),
                       ),
                       SizedBox(height: 10),
                       Container(
+                        padding: EdgeInsets.only(left: 20),
                         height: 150,
                         child: ListView(
                           scrollDirection: Axis.horizontal,
@@ -141,15 +150,19 @@ class DetailPage extends StatelessWidget {
                         ),
                       ),
                       SizedBox(height: 10),
-                          Text(
+                      Text(
                         'References',
                         style: TextStyle(
                             fontSize: 18, fontWeight: FontWeight.w500),
                       ),
-                         SizedBox(height: 10),
-                      Card(child: ListTile(title: Text("Wikipeida Page"),))
-
-
+                      SizedBox(height: 10),
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 20),
+                        child: Card(
+                            child: ListTile(
+                          title: Text("Wikipeida Page"),
+                        )),
+                      )
                     ],
                   ),
                 ],
